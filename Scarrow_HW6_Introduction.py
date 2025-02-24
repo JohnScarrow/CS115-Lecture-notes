218ecdc3c6e6b609e3ab8d99c60c80cd4527f984
John Scarrow <johna@scarrows.com>
2025-02-24 18:59:16 +0000

    Fixing rebase with github

---
+++ Scarrow_HW6_Introduction.py
@@ -0,0 +1,21 @@
+'''
+JohnScarrow
+2/19/2025
+First Assignment
+
+'''
+# Comments
+print("lets add some numbs")
+
+num1 = int(input("First numb "))
+num2 = int(input("Second numb "))
+
+#Making the 4 required operators
+sum = num1 + num2
+diff = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+#Posting operators to user
+
+print(f'|| Sum = {sum} || Difference = {diff} || Product = {product} || Quotient = {quotient} ||' )
